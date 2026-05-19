daac71ac893df1c362449d1571fa0ba9855cb241
Phoebe Chao <phoebejao0725@gmail.com>
2026-05-19 18:16:26 +0100

    Initial Commit

---
+++ DealMates/DealMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealMatesApp.swift
+//  DealMates
+//
+//  Created by EleaneJao on 19/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DealMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
